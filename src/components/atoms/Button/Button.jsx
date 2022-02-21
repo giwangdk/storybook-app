ca3119f83b185d860ae9091347ff7343e7ceffe3
gidwikintan <gidwikintan@gmail.com>
2022-02-21 21:54:46 +0700

    layouting button and add proptypes

--- src/components/atoms/Button/Button.jsx
+++ src/components/atoms/Button/Button.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import classnames from 'classnames'
+import styles from './index.css';
+import PropTypes from 'prop-types'
 
-function Button({children}) {
+function Button(props) {
+  const { type, onClick, disabled, className, label, size} = props
+  
+  const classProps = classnames(
+    styles.button,
+    size,
+    className
+  )
+
   return (
-    <button className="p-2 bg-gray-700">{children}</button>
+    <button className={classProps} onClick={onClick} type={type} disabled={disabled}>{label}</button>
   )
 }
 
+
+Button.propTypes = {
+  type: PropTypes.oneOf(['submit', 'reset']),
+  onClick: PropTypes.func,
+  disabled: PropTypes.bool,
+  className: PropTypes.string,
+  label: PropTypes.string,
+  size : PropTypes.oneOf(['small', 'medium', 'large'])
+}
 export default Button
